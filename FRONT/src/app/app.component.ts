83da36d32af16b91715d44e624d1f4e7795ff2e5
Hainaku <hainaku@Hainakus-Mini.home>
2018-02-15 22:00:38 +0000

    material desing front

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  constructor(){
+
+  }
+ngAfterViewInit(){
+  this.receivedEvent('deviceready');
+}
+receivedEvent(id){
+  var parentElement = document.getElementById(id);
+  var listeningElement = parentElement.querySelector('.listening');
+  var receivedElement = parentElement.querySelector('.received');
+
+  listeningElement.setAttribute('style', 'display:none;');
+  receivedElement.setAttribute('style', 'display:block;');
+
+  console.log('Received Event: ' + id);
+}
 }
